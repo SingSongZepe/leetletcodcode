bdb02d66d29122b1b6f25c223fcd63515cb58a7b
sszp <aaaaaa1964027610@163.com>
2024-05-17 05:53:58 +0800

    98

--- lc98/src/test.rs
+++ lc98/src/test.rs
@@ -6,7 +6,17 @@ mod tests {
 	
 	#[test]
 	fn test1() {
-		
+		let v = vec![2, 1, 3];
+		let tree = helper(v);
+		let result = Solution::is_valid_bst(tree);
+		println!("{}", result);
 	}
 
+	#[test]
+	fn test2() {
+		let v = vec![5, 1, 4, -1, -1 ,3, 6];
+		let tree = helper(v);
+		let result = Solution::is_valid_bst(tree);
+		println!("{}", result);
+	}
 }
